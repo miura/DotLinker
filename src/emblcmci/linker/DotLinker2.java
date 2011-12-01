8122556cc877af9e8ca14b4a382d6133c8494811
Kota@mac <miura@embl.de>
2011-12-01 03:47:40 +0100

    dotlinker2 added

---
+++ src/emblcmci/linker/DotLinker2.java
@@ -0,0 +1,62 @@
+package emblcmci.linker;
+/**
+ * modified methods for importing Volocity 3D data
+ * 
+ * Mette
+ * Kota Miura (miura@embl.de)
+ */
+import java.util.Arrays;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.measure.ResultsTable;
+
+public class DotLinker2 extends DotLinker{
+	
+	int znum = 27; //this should be at some point be given
+	
+	public DotLinker2(ImagePlus imp) {
+		super(imp);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public StackFrames[] dataloader(){
+		
+		//data loading from results table
+		ResultsTable rt = ResultsTable.getResultsTable();
+		if (rt == null){
+			IJ.error("no  results table !");
+			return null;
+		}
+		if (rt.getColumn(0).length <10){
+			IJ.error("there seems to be almost no data...");
+			return null;
+		}
+		
+		float[] xA = rt.getColumn(rt.getColumnIndex("Centroid X"));
+		float[] yA = rt.getColumn(rt.getColumnIndex("Centroid Y"));
+		float[] zA = rt.getColumn(rt.getColumnIndex("Centroid Z"));
+//		float[] areaA = rt.getColumn(rt.getColumnIndex("Area"));
+		float[] timeA = rt.getColumn(rt.getColumnIndex("Timepoint"));
+//		float[] sliceAsort = sliceA.clone();
+//		Arrays.sort(sliceAsort);
+//		int startframe = (int) sliceAsort[0];
+//		int endframe = (int) sliceAsort[sliceAsort.length-1];
+		int startframe = (int) timeA[0];
+		int endframe = (int) timeA[timeA.length-1];		
+		int framenumber = endframe - startframe + 1;
+		frameA = new StackFrames[framenumber];
+		for (int i = 0; i < framenumber; i++){
+			frameA[i] = new StackFrames(i);
+			//frameA[i].particles.next = new Particle[linkrange];
+		}
+		// fill in the Myframe object
+		for (int i = 0 ; i< timeA.length; i++){
+			Particle3D particle3d = new Particle(xA[i], yA[i], (int) (timeA[i] - 1), areaA[i], i);
+			frameA[particle.frame].particles.add(particle);
+		}
+		return frameA;
+	}
+
+
+}
