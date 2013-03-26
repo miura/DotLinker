36e84a96c5fc10fcdb721c879349fe5327d600f3
Kota Miura <miura@embl.de>
2013-03-26 18:59:57 +0100

    small typo

--- src/main/java/emblcmci/obj/Tracks2Dcells.java
+++ src/main/java/emblcmci/obj/Tracks2Dcells.java
@@ -1,10 +1,5 @@
 package emblcmci.obj;
 
-
-import java.util.Collection;
-
-import emblcmci.linker.LinkAnalyzer;
-
 public class Tracks2Dcells extends AbstractTracks{	
 	
 	public AbstractTracks addTrack(int ID, Track2Dcells t){
@@ -12,6 +7,4 @@ public class Tracks2Dcells extends AbstractTracks{
 		t.setTrackID(ID);
 		return this;
 	}
-
-	
 }
