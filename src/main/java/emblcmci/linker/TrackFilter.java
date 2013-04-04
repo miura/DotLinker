72f874e885b596aef21473d009ba90da0e719ea1
Kota Miura <miura@embl.de>
2013-04-04 20:49:30 +0200

    Track filter by length added

---
+++ src/main/java/emblcmci/linker/TrackFilter.java
@@ -0,0 +1,25 @@
+package emblcmci.linker;
+
+import emblcmci.obj.AbstractTrack;
+import emblcmci.obj.Tracks;
+
+/**
+ * go through tracks and take only long tracks.
+ *  
+ * 
+ * @author miura
+ *
+ */
+public class TrackFilter {
+	
+	public Tracks run(Tracks ts, int lengthThreshold){
+		Tracks filtered = new Tracks();
+		for (AbstractTrack t : ts.values()){
+			if (t.getNodes().size() > lengthThreshold){
+				filtered.addTrack(t.getTrackID(), t);
+			}
+		}
+		return filtered;	
+	}
+	
+}
