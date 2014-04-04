ac89361bb86d670e73a1edf5d4f90e42bbe84577
Kota Miura <miura@embl.de>
2014-04-04 15:26:08 +0200

    Jar update and script working with file path to particle analysis results csv file and outputs resultstable object with tracks.

--- src/test/scripts/DotLinker2014.py
+++ src/test/scripts/DotLinker2014.py
@@ -1,8 +1,19 @@
 from de.embl.cmci.linker import DotLinker
+from de.embl.cmci.io import ResultsTableLoader, DataWriterArea2D
 from de.embl.cmci.linker.costfunctions import LinkCostswithAreaDynamics
+from ij.measure import ResultsTable
 
 print "test"
 lcAD = "de.embl.cmci.linker.costfunctions.LinkCostswithAreaDynamics"
-dotlinker = DotLinker('de.embl.cmci.io.ResultsTableLoader')
-linkcostfunction = dotlinker.setLinkCostFunction(lcAD);
-linkcostfunction.setParameters(10.0, 2.0);
+loadmethod = 'de.embl.cmci.io.ResultsTableLoader'
+path = '/Users/miura/Dropbox/people/Giogia_Stefano/toJoe/coords.csv'
+
+rt = ResultsTable.open2(path)
+dotlinker = DotLinker(loadmethod, rt)
+dotlinker.setTrajectoryThreshold(5)
+dotlinker.setShowTrackTable(False)
+#dotlinker = DotLinker(loadmethod)
+linkcostfunction = dotlinker.setLinkCostFunction(lcAD)
+linkcostfunction.setParameters(5.0, 2.0)
+rtout = dotlinker.doLinking(False)
+rtout.show("Tracks")
