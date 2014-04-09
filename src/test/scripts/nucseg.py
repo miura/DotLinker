0eacead434cacb6c4b679297e8d5ac1c4c1da468
Kota Miura <miura@embl.de>
2014-04-09 12:04:40 +0200

    nucseg.py fixed for additional argument in .binarize

--- src/test/scripts/nucseg.py
+++ src/test/scripts/nucseg.py
@@ -5,6 +5,6 @@ imp = IJ.openImage("http://imagej.nih.gov/ij/images/blobs.gif");
 #imp = IJ.openImage("http://imagej.nih.gov/ij/images/m51.zip");
 imp = IJ.openImage('/Volumes/D/Julia20130201-/NucleusSegmentationStudy/20130320/L1CH2_maxp_300-374-6.tif')
 nsp = NSP()
-res = nsp.binarize(imp.getProcessor());
+res = nsp.binarize(imp.getProcessor(), 0.1);
 ImagePlus("result", res).show()
 
